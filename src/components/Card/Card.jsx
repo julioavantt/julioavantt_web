9a0c1a504b2321ebfaff400eed7258bc9b60e956
Juliana <yo@me.amo>
2025-10-31 23:16:12 -0300

    feat: recommendations

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,22 @@
+import styles from "./Card.module.css";
+
+export const Card = ({ item }) => {
+  return (
+    <section className={styles.card}>
+      <div className={styles.header}>
+        <div className={styles.avatar_wrapper}>
+          <img
+            src={`/assets/img/testimonials/${item.avatar}`}
+            alt={item.name}
+            className={styles.avatar}
+          />
+        </div>
+        <div>
+          <h4>{item.name}</h4>
+          <p className={styles.role}>{item.role}</p>
+        </div>
+      </div>
+      <blockquote className={styles.content}>{item.recommendation}</blockquote>
+    </section>
+  );
+};
