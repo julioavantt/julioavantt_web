32f8878201e78570bd0fdc08b2f6f8e6ba1be23a
unicorn <unicorn@flying.com>
2025-11-02 20:11:10 -0300

    refactor: home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,20 +5,14 @@ export const Home = () => (
     <div className={styles.about}>
       <h1>Julio Avantt!</h1>
       <h2>
-        Frontend & React Senior developer | Mentor, Profesor y contenidista:
-        Fundamentos de IA, React, Front End, Javascript, CSS, HTML, Git |
-        Director canal YouTube @juniorpride
+        Enseño tecnología a las generaciones que dominarán el futuro.
       </h2>
       <p>
-        Experto en Front End en empresas internacionales +17 | Autodidacta de la
-        IA | Apasionado por recibir y transmitir conocimientos | Creador de
-        video curso y materias en React, HTML, CSS, Javascript, Git | Profesor y
-        mentor en React, Git, Javascript, CSS y HTML | Líder | Director canal de
-        YouTube @juniorpride | Comunicador
+        Apasionado por compartir conocimiento en Programación, IA y Data Science, combinando docencia, liderazgo y creación de contenido.
       </p>
     </div>
     <div className={styles.profile_img_wrapper}>
-      <img src="/assets/img/julio-avantt-profile.png" alt="Julio Avantt!" />
+      <img src="/assets/img/profile.jpeg" alt="Julio Avantt!" />
     </div>
   </div>
 );
