d3d2047b54c17bca9fd3c09b50713f117817ef7a
Juliana <yo@me.amo>
2025-11-03 03:00:13 -0300

    refine: youtube imgs

--- src/components/YouTubeCard/YoutubeCard.jsx
+++ src/components/YouTubeCard/YoutubeCard.jsx
@@ -2,11 +2,18 @@ import styles from "./YouTubeCard.module.css";
 
 export const YouTubeCard = ({ item }) => (
   <a href={item.url} target="_blank" className={styles.card}>
-    <img
-      src={`/assets/img/youtube/${item.img}`}
-      alt={item.title}
-      className={styles.img}
-    />
+    <picture>
+      <source
+        srcset={`/assets/img/youtube/${item.img}.avif`}
+        type="image/avif"
+      />
+      <img
+        src={`/assets/img/youtube/${item.img}.jpg`}
+        alt={item.title}
+        loading="lazy"
+        className={styles.img}
+      />
+    </picture>
     <div>
       <h4>{item.title}</h4>
     </div>
